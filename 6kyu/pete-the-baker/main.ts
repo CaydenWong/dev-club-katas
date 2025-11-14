7ffcfcd5ee92bd1973213396cc2817ca8d78284e
caydenwong <1460901+CaydenWong@users.noreply.github.com>
2025-11-14 09:52:16 +0000

    refactored

--- 6kyu/pete-the-baker/main.ts
+++ 6kyu/pete-the-baker/main.ts
@@ -2,10 +2,13 @@ type Recipe = { [key: string]: number };
 type Ingradients = { [key: string]: number };
 
 export const cakes = (recipe: Recipe, available: Ingradients): number => {
-    const maxByGradient = Object.keys(recipe).map((key) => {
-        const availableAmount = available[key] || 0;
-        const requiredAmount = recipe[key];
-        return Math.floor(availableAmount / requiredAmount);
-    });
-    return Math.min(...maxByGradient);
+    const cakesByGradient = Object.keys(recipe).map((key) => 
+        maxUnitsFromResource(recipe[key], available[key])
+    );
+    return Math.min(...cakesByGradient);
 };
+
+export const maxUnitsFromResource = (requiredAmount: number, availableAmount: number): number => {
+    if (!availableAmount) return 0;
+    return Math.floor(availableAmount / requiredAmount);
+}
